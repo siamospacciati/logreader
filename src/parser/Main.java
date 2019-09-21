bcabd334daa153dfb8079c81dde9779d5a82d685
chicant <chicant@CCs-MBP.local>
2019-09-21 20:48:37 +0200

    Created package parser with main

---
+++ src/parser/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package parser;
+
+/**
+ * @author chicant
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
